c6d3f3bfe0f87cde0a54e309b6d91a51099be001
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-07-06 21:13:52 -0500

    variable array for questions and answers added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,26 @@ var rightAnswers = 0;
 var wrongAnswers = 0;
 var didNotAnswer = 0;
 var timer = 20;
-
 var intervalId;
+
+// need to store the questions and answers in an array
+var questions = [{
+    question: "What video game company is associated with Super Mario Bros.?",
+    guesses: ["Nintendo", "Sega", "Sony", "Microsoft"],
+    answer: 0
+},{
+    question: "This lady of action uses guns, solves puzzles, and had movies based on her adventures.",
+    guesses: ["Laura Croft", "Princess Peach", "Link", "Aerith"],
+    answer: 0
+},{
+    question: "What acronym best describes the game World of Warcraft",
+    guesses: ["MMORPG", "MOBA", "RPG", "TBS"],
+    answer: 0
+},{
+    question: "Capcom's Megaman video game had an arch nemesis named?",
+    guesses: ["Dr. Wiley", "Eggman", "Bowser", "Thanos"],
+    answer: 0
+}];
 
 // need to start the countdown timer
 function startTimer() {
@@ -32,10 +50,17 @@ function reduce() {
     }
 
 // questions to be produced when the button is clicked
+$('#start').on('click', function() {
+
+    $('#time').html('<h2>Vault 111 will close in: 20 Seconds</h2>' + '<br>');
+
+    startTimer();
+    // start button function should produce the questions
 
 
+});
 
-// start button function should produce the questions
+
 
 
 // submit button for when the questions are answered by the user
