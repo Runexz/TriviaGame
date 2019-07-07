b1f670cdb6e162901a0494915d80b00e69035a0f
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-07-06 20:03:28 -0500

    pseudocode js with some variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,37 @@
 
+// need a list of variables for the right, wrong, didnt answer, and timer
+var rightAnswers = 0;
+// console.log("Correct answer " + rightAnswers);
+var wrongAnswers = 0;
+var didNotAnswer = 0;
+var timer = 20;
+
+var intervalId;
+
+// need to start the countdown timer
+
+
+
+// need to display the countdown timer numbers in html
+
+
+// need to function for the timer
+
+
+// questions to be produced when the button is clicked
+
+
+
+// start button function should produce the questions
+
+
+// submit button for when the questions are answered by the user
+
+
+// need to store the right, wrong, unanswered and display to html
+
+
+// need to hide the questions and display the results
+
+
+
